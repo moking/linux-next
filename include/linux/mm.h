709405de6b518dedcc32f01fbece480514235154
Matthew Wilcox (Oracle) <willy@infradead.org>
2025-02-21 14:31:00 +0000

    mm: Add folio_mk_pmd()
    Removes five conversions from folio to page.  Also removes both callers
    of mk_pmd() that aren't part of mk_huge_pmd(), getting us a step closer to
    removing the confusion between mk_pmd(), mk_huge_pmd() and pmd_mkhuge().
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2014,7 +2014,24 @@ static inline pte_t folio_mk_pte(struct folio *folio, pgprot_t pgprot)
 {
 	return pfn_pte(folio_pfn(folio), pgprot);
 }
+
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+/**
+ * folio_mk_pmd - Create a PMD for this folio
+ * @folio: The folio to create a PMD for
+ * @pgprot: The page protection bits to use
+ *
+ * Create a page table entry for the first page of this folio.
+ * This is suitable for passing to set_pmd_at().
+ *
+ * Return: A page table entry suitable for mapping this folio.
+ */
+static inline pmd_t folio_mk_pmd(struct folio *folio, pgprot_t pgprot)
+{
+	return pmd_mkhuge(pfn_pmd(folio_pfn(folio), pgprot));
+}
 #endif
+#endif /* CONFIG_MMU */
 
 /**
  * folio_maybe_dma_pinned - Report if a folio may be pinned for DMA.

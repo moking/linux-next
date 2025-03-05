d11f3d36ae8ccce2ed67f437cefea3dd9415ebea
Fan Ni <fan.ni@samsung.com>
2025-03-04 17:04:15 -0800

    fs/squashfs: Convert &folio->page to folio_page(folio, 0)
    This is part of the efforts to move some fields out of struct page to
    reduce its size.
    Convert the usage of &folio->page under "fs/squashfs".
    
    Signed-off-by: Fan Ni <fan.ni@samsung.com>

--- fs/squashfs/file_direct.c
+++ fs/squashfs/file_direct.c
@@ -22,7 +22,7 @@
 int squashfs_readpage_block(struct folio *folio, u64 block, int bsize,
 		int expected)
 {
-	struct page *target_page = &folio->page;
+	struct page *target_page = folio_page(folio, 0);
 	struct inode *inode = folio->mapping->host;
 	struct squashfs_sb_info *msblk = inode->i_sb->s_fs_info;
 	loff_t file_end = (i_size_read(inode) - 1) >> PAGE_SHIFT;
